547fb3c17b19166476f93347a419cc45e0c28a9f
Anzerus <Anzerus@mail.ru>
2018-04-11 18:14:56 +0300

    s
    ws

---
+++ 27/Project/Assets/skripts/timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	float sec = 0;
+	void Update () {
+		sec=sec + Time.deltaTime;
+		print((int)sec);
+	}
+}
