91d676fe6e70153a5d7760a485e6ec99a491f888
Anzerus <Anzerus@mail.ru>
2018-04-18 18:28:29 +0300

    i
    8

---
+++ 27/usa/Assets/unity/ScriptWave.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptWave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		print("Hello world!");
+	
+	}
+	
+	// Update is called once per frame
+	float wave=0;
+	float sec=0;
+	void Update () {
+		
+		float second = Time.deltaTime;
+		sec =sec + second;
+		if (sec > 5)
+		{
+			print ("новая волна");
+			wave = wave+1;
+			print (wave);
+
+			sec=0;
+		}
+			
+	}
+}
---
+++ 27/usa2/Assets/Skripts/Nuda.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nuda : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public Transform monster;
+	void Update () {
+		float dist = Vector3.Distance(monster.position, transform.position);
+		print (dist);
+	}
+}
