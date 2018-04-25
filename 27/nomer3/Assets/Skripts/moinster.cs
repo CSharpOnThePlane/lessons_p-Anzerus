b0e8af6fcf0d739e4c2b4f4c4078d95cf6abf200
Anzerus <Anzerus@mail.ru>
2018-04-25 18:25:40 +0300

    gg
    gg

---
+++ 27/nomer3/Assets/Skripts/moinster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moinster : MonoBehaviour {
+	public Transform enemy;
+	// Use this for initialization
+	void Start () {
+		print (enemy.position);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+
+		float dist = Vector3.Distance (enemy.position, transform.position);
+		if (dist<10) {
+			print ("осторожно");
+		}
+	}
+}
---
+++ 27/nomer3/Assets/Skripts/skript3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skript3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	float time = 20;
+	void Update () {
+		
+		time = time - Time.deltaTime;
+		if (time>0) {
+			print ((int)time);
+		}
+
+		if (time<0) {
+			print ("время вышло");
+		}
+	}
+
+}
